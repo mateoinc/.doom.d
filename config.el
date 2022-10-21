5ba5d01d47e06e159be40286bc8eb62ead8756de
Mateo Barria-Urenda <mateobarria@gmail.com>
2022-10-21 10:52:01 +0200

    Changed theme to Dracula

--- config.el
+++ config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-snazzy)
+(setq doom-theme 'doom-dracula)
 
 ;; Change the logo
 (setq fancy-splash-image (concat doom-user-dir "M-x_butterfly.png"))
