b740c0f4f77222c44e2824a56bd84d0a0b0f4a11
Mateo Barría <mateobarria@gmail.com>
2023-04-04 10:57:46 -0400

    Reordered theming

--- config.el
+++ config.el
@@ -32,7 +32,6 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'catppuccin)
 (setq catppuccin-flavor 'frappe) ;; 'frappe or 'latte, 'macchiato, or 'mocha
 (setq doom-theme 'catppuccin)
 ;; (catppuccin-reload)
