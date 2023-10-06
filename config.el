eb3808e96b6cac948f9cad85e2e6e9c01a041fd9
Mateo Barría <mateobarria@gmail.com>
2023-10-06 16:13:54 -0300

    Changed logo

--- config.el
+++ config.el
@@ -41,7 +41,7 @@
 (timeclock-mode-line-display)
 
 ;; Change the logo
-(setq fancy-splash-image (concat doom-user-dir "ZTMY.png"))
+(setq fancy-splash-image (concat doom-user-dir "CyberpunkGirl.png"))
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
